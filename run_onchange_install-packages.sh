93959ac61cecb53cd7c78f864095311a7d458f2e
Nina Morgan <nina@projectmakeit.com>
2023-05-20 07:16:36 -0400

    update vim to neovim

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo apt install zsh -y
+sudo apt install zsh wget curl -y
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O ~/.local/bin/nvim.appimage
+chmod u+x ~/.local/bin/nvim.appimage
